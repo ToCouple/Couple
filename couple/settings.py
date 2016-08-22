2fd81df17cf3687b73b3d83fb7ecc198f73f3412
Puppetplay <airkiss81@gmail.com>
2016-08-22 16:11:53 +0900

    부트스트랩화면설정 Static설정

--- couple/settings.py
+++ couple/settings.py
@@ -118,8 +118,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_statics')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '../static'),
-    './static/',
+    os.path.join(BASE_DIR, 'static'),
 )
